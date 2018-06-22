312c222e7a6ca7d4400acf4a539cc372476d6b42
Konstantin Gazha <konstgazha@yandex.ru>
2018-06-22 20:43:33 +0500

    Imporve VKManager. Create auth method

--- linkedfriends.py
+++ linkedfriends.py
@@ -7,47 +7,55 @@ from functools import reduce
 
 
 class VKManager:
-    def __init__(self, login, password):
-        self.login = login
-        self.password = password
+    def __init__(self):
         self.redirect_uri = "https://oauth.vk.com/blank.html"
         self.url_auth = 'https://oauth.vk.com/authorize?client_id={0}&display=page&redirect_uri={1}&scope=friends&respоnse_type=token'.format(client_id, self.redirect_uri)
+        self.login = ""
+        self.password = ""
+        self.crawler = ""
     
-    def get_new_access_token(self, client_id, client_secret):
-        crawler = crawlers.SeleniumCrawler()
-        crawler.driver.get(self.url_auth)
-        crawler.wait_loading_by_class('oauth_from_input')
-        
-        login_fields = crawler.driver.find_elements_by_class_name('oauth_form_input')
+    def auth(self, login, password):
+        self.login = login
+        self.password = password
+        if not self.crawler:
+            self.crawler = crawlers.SeleniumCrawler()
+        self.crawler.driver.get(self.url_auth)
+        self.crawler.wait_loading_by_class('oauth_from_input')
+        login_fields = self.crawler.driver.find_elements_by_class_name('oauth_form_input')
         login_fields[0].send_keys(self.login)
         login_fields[1].send_keys(self.password)
-        crawler.driver.find_element_by_id('install_allow').click()
+        self.crawler.driver.find_element_by_id('install_allow').click()
         try:
-            crawler.driver.find_element_by_class_name('button_indent').click()
+            self.crawler.driver.find_element_by_class_name('button_indent').click()
         except:
             pass
-        
-        response_url_auth = crawler.driver.current_url
+
+    def get_new_access_token(self, client_id, client_secret):
+        if not self.crawler:
+            self.crawler = crawlers.SeleniumCrawler()
+            raise('OMG')
+        response_url_auth = self.crawler.driver.current_url
         code = response_url_auth.split('=')[-1]
         url_access_token = 'https://oauth.vk.com/access_token?client_id={0}&client_secret={1}&redirect_uri={2}&code={3}'
-        soap_access_token = crawler.get_soup(url_access_token.format(client_id, client_secret, self.redirect_uri, code))
+        soap_access_token = self.crawler.get_soup(url_access_token.format(client_id, client_secret, self.redirect_uri, code))
         access_token = json.loads(soap_access_token.text)['access_token']
-        crawler.close()
+        self.crawler.close()
         return access_token
 
 
 def get_friends_intersection(users):
     return reduce(set.intersection, map(set, users))
 
-# login = ""
-# password = ""
-# client_id = ""
-# client_secret = ""
+login = ""
+password = ""
+client_id = ""
+client_secret = ""
 
-# vkm = VKManager(login, password)
-# token = vkm.get_new_access_token(client_id, client_secret)
-# print(token)
-access_token = ""
+vkm = VKManager()
+vkm.auth(login, password)
+access_token = vkm.get_new_access_token(client_id, client_secret)
+print(access_token)
+# access_token = ""
 
 vk_api = vkapi.VKAPI(access_token)
 friends = vk_api.get_user_friends("")
